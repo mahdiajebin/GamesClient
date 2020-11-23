8605cd4dd89a6a0a5e1b37c1e02106bedb631a21
mahdiajebin <mahdiajebin@gmail.com>
2020-11-23 02:38:02 -0500

    changes made to fxml and controller

--- src/main/java/edu/citytech/cst/game/Connect4/connect4Controller.java
+++ src/main/java/edu/citytech/cst/game/Connect4/connect4Controller.java
@@ -15,9 +15,20 @@ public class connect4Controller implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+    //counter
         for(int i = 0; i<42; i++){
-            Label l = new Label("test" + i);
+            Label l = new Label("" + i);
+            if(i>20){
+                l.getStyleClass().add("highlight");
+            }
+            //which label was clicked
+            l.setOnMouseClicked(e-> {
+            Label label =(Label) e.getSource();
+                label.getStyleClass().add("highlight");
+                System.out.println(e);
+            });
+
+
             fpSlots.getChildren().add(l);
         }
 
